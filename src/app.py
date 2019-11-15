25824b53c73f5fb5a004870b443530cee72840f2
Nurbek Saidnassim <nurbek.saidnassim@outotec.com>
2019-11-15 14:46:57 +0200

    added more routes and ajax calls for all the methods.

--- src/app.py
+++ src/app.py
@@ -1,19 +1,32 @@
 from flask import Flask, request, render_template
 from Model import Model
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/api", methods=["GET", "POST"])
+
+
+@app.route("/addlayer", methods=["GET", "POST"])
 def AddLayer():
     print(request.json)
     return "OK", 200
 
+@app.route("/summary", methods=["GET", "POST"])
+def Summary():
+    print(request.json)
+    return "OK", 200
+
+@app.route("/save", methods=["GET", "POST"])
+def SaveModel():
+    print(request.json)
+    return "OK", 200
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("../templates/index.html")
+    return render_template("index.html")
 
 
 
 if __name__ == '__main__':
+    model = Model(10)
     app.run(host="127.0.0.1", port=5000, debug=True)
